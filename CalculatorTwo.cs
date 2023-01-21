36989d7a5dae062f4caf0872e0ecbabbf67a5eff
cedriclv <[cedriclevalegant@gmail.com](mailto:cedriclevalegant@gmail.com)>
2023-01-21 17:17:15 +0100

    quete fonctions

---
+++ CalculatorTwo.cs
@@ -0,0 +1,87 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        double result = 0;
+
+        Console.WriteLine("Enter a first number :");
+        double numberOne = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter a second number :");
+        double numberTwo = Convert.ToDouble(Console.ReadLine());
+
+        int operationId = -1;
+
+        while (operationId < 1 || operationId > 5)
+        {
+            operationId = AskOperator();
+            result = CallOperator(operationId, numberOne, numberTwo);
+        }
+
+        Console.WriteLine("The result is : " + result);
+
+    }
+
+    public static int AskOperator()
+    {
+        Console.WriteLine("Enter the chosen operation :");
+        Console.WriteLine("1: Addition");
+        Console.WriteLine("2: Substraction");
+        Console.WriteLine("3: Multplication");
+        Console.WriteLine("4: Division");
+        Console.WriteLine("5: Modulo");
+        int operationId = Convert.ToInt32(Console.ReadLine());
+        return operationId;
+    }
+
+    public static double CallOperator(int operationId, double numberOne, double numberTwo)
+    {
+        double result = 0;
+
+        switch (operationId)
+        {
+            case 1:
+                result = Add(numberOne, numberTwo);
+                break;
+            case 2:
+                result = Substract(numberOne, numberTwo);
+                break;
+            case 3:
+                result = Multiply(numberOne, numberTwo);
+                break;
+            case 4:
+                result = Divide(numberOne, numberTwo);
+                break;
+            case 5:
+                result = Modulo(numberOne, numberTwo);
+                break;
+            default:
+                Console.WriteLine("Wrong input");
+                break;
+        }
+        return result;
+    }
+
+    public static double Add(double firstFigure, double secondFigure)
+    {
+        return firstFigure + secondFigure;
+    }
+    public static double Substract(double firstFigure, double secondFigure)
+    {
+        return firstFigure - secondFigure;
+    }
+    public static double Multiply(double firstFigure, double secondFigure)
+    {
+        return firstFigure * secondFigure;
+    }
+    public static double Divide(double firstFigure, double secondFigure)
+    {
+        return firstFigure / secondFigure;
+    }
+    public static double Modulo(double firstFigure, double secondFigure)
+    {
+        return firstFigure % secondFigure;
+    }
+}
